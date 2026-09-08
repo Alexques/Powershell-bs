a103e34379bb3e1c1dac319dbfb4d99e3e5e53c7
AlexQuest <93671796+Alexques@users.noreply.github.com>
2026-09-08 13:01:23 +0200

    Delete new secudlua.ps1

--- new secudlua.ps1
+++
@@ -1,7 +0,0 @@
-﻿$Action = New-ScheduledTaskAction `
-    -Execute "PowerShell.exe" `
-    -Argument '-ExecutionPolicy Bypass -File "C:\Users\AleksanderVestveitOl\OneDrive - Informasjonsteknologi og Medieproduksjon\Skrivebord\powershell skrips\Kalender skript.ps1"'
-
-$Trigger = New-ScheduledTaskTrigger -Daily -At "07:50 AM"
-
-Register-ScheduledTask -TaskName "MorningRoutine" -Action $Action -Trigger $Trigger
